d49c4ba0ca0f18652c1782dfdd2b4bf89a940fc0
MateuszOszczypala <89586245+MateuszOszczypala@users.noreply.github.com>
2025-03-07 07:59:06 +0100

    Update NSGA_II_CS5.m

--- aux_functions/NSGA_II_CS5.m
+++ aux_functions/NSGA_II_CS5.m
@@ -22,7 +22,7 @@ K = sum(k);
 
 % lambda failure rates for active component
 
-%working_failure_rate = round(0.5*rand(10,1), 2);
+%working_failure_rate = round(0.5+0.5*rand(10,1), 2);
 %warm_standby_failure_rate = round((0.2*rand(1)+0.1)*working_failure_rate, 2);
 working_failure_rate = [0.86 0.98 0.65 0.57 0.63 0.70 0.55 0.54 0.61 0.57];
 warm_standby_failure_rate = [0.26 0.28 0.06 0.28 0.18 0.22 0.12 0.12 0.16 0.14];
@@ -36,24 +36,20 @@ warm_standby_switching_rate = [7.36 9.88 7.24 9.14 9.43 8.71 9.72 9.76 8.97 7.58
 % mu repiar rates
 % repair_rate = round(2+rand(10,1), 2);
 repair_rate = [2.18 2.04 2.11 2.62 2.94 2.35 2.41 2.98 2.95 2.68];
-%{
-for i = 1:10
-    unit_cost(i) = round(0.2*(10-i)+rand(1), 2);
-    unit_weight(i) = round(0.1*(10-i)+rand(1), 2);
-end
-%}
 
 % unit cost
+% unit_cost = round(3*rand(10,1), 2)
 unit_cost = [2.79 1.94 1.64 1.88 1.41 1.55 1.15 0.91 0.92 0.35];
 
 % unit weight
+% unit_weight = round(2*rand(10,1), 2)
 unit_weight = [1.67 1.46 1.00 1.13 1.10 0.98 0.88 0.28 1.10 0.97];
 
 % population size
 population_size = 200;
 
 % number of generation
-max_iteration = 1000;
+max_iteration = 10000;
 
 % size declaration
 n(1:m,1:population_size,1:max_iteration) = NaN; % total number of components in subsystem
