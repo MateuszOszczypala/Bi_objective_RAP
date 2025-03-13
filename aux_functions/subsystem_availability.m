ccef0689406e4505511b8cb31d344af89d72d708
MateuszOszczypala <89586245+MateuszOszczypala@users.noreply.github.com>
2025-03-13 13:58:52 +0100

    Update subsystem_availability.m

--- aux_functions/subsystem_availability.m
+++ aux_functions/subsystem_availability.m
@@ -5,13 +5,12 @@ function Subsystem_availabilities = subsystem_availability(n, k, Ergodic_prob, r
     
     elseif redundancy_strategy == 1 % warm standby
         Subsystem_availabilities = sum(Ergodic_prob(1:n-k+1));
+
+     elseif redundancy_strategy == 2 % mixed standby
+        Subsystem_availabilities = sum(Ergodic_prob(1:n-k+1));   
     
-    elseif redundancy_strategy == 2 % hot standby        
-        Subsystem_availabilities = sum(Ergodic_prob(1:2*n-2*k+1));
-        
-    elseif redundancy_strategy == 3 % mixed standby
-        Subsystem_availabilities = sum(Ergodic_prob(1:n-k+1));
-    
-    
+    elseif redundancy_strategy == 3 % hot standby        
+        Subsystem_availabilities = sum(Ergodic_prob(1:2*n-2*k+1));      
+           
     end
 end
